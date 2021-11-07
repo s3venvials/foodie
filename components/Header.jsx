b84d3a7bef95b0fff1c57088c82499f7c5712465
testcerium <testcerium.io@gmail.com>
2021-11-06 18:55:02 -0700

    Add meta props to head

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,16 @@ export default function Header() {
     <Head>
       <title>Foodie Cuisines</title>
       <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <meta property="og:image" content="https://res.cloudinary.com/frontndev/image/upload/v1636249633/foodiecuisines/final_61872fbd8a744300a69c4096_718161.png"/>
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/frontndev/image/upload/v1636249633/foodiecuisines/final_61872fbd8a744300a69c4096_718161.png"
+      />
+      <meta content="Foodie Cuisines" property="og:title" />
+      <meta
+        content="View, create and share food recipes"
+        property="og:description"
+      />
+      <meta content="https://foodiecuisines.com" property="og:url" />
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
